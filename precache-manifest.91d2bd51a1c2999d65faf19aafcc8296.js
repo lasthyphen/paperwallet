b7b2edd98b0abede8e094109a681e3dbc9bbe082
Saleem Farid <davidhill0173@gmail.com>
2024-11-14 01:47:10 +0000

    first commitr

---
+++ precache-manifest.91d2bd51a1c2999d65faf19aafcc8296.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252d7e2ca1d0624020fe",
+    "url": "/css/app.90abaa45.css"
+  },
+  {
+    "revision": "db5877375cfe95cfb9f4",
+    "url": "/css/chunk-vendors.dfc4150c.css"
+  },
+  {
+    "revision": "2a64b7e331c7811aefa6",
+    "url": "/css/message.56e37ab0.css"
+  },
+  {
+    "revision": "b0e128b85fac9d4ef015",
+    "url": "/css/message:sign.fd5b4b94.css"
+  },
+  {
+    "revision": "d7452dd851606b09e836",
+    "url": "/css/message:verify.744797de.css"
+  },
+  {
+    "revision": "36809e6657759a5e2980",
+    "url": "/css/wallet.360c5b17.css"
+  },
+  {
+    "revision": "db68e031749c721f114f",
+    "url": "/css/wallet:entropy.f2dff00a.css"
+  },
+  {
+    "revision": "b0f5dc43fd5b39f094bf",
+    "url": "/css/wallet:passphrase.744797de.css"
+  },
+  {
+    "revision": "14c1316631d0f79f0ef09eab60be3222",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "6d0dca36f4b308f1b05ac34f8b18c8d7",
+    "url": "/fonts/DankMono-Regular.6d0dca36.ttf"
+  },
+  {
+    "revision": "46341b1dfc2e152fe5d5a4c771e307f1",
+    "url": "/fonts/FeyCalibre-Bold.46341b1d.woff"
+  },
+  {
+    "revision": "5f30d28e8d36298d1767534f3611df86",
+    "url": "/fonts/FeyCalibre-Light.5f30d28e.woff"
+  },
+  {
+    "revision": "412d38726b418bb44f127842eaeef49b",
+    "url": "/fonts/FeyCalibre-Regular.412d3872.woff"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/fonts/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/fonts/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/fonts/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "4ac65b8eed2faddcf2d9ecf529559d50",
+    "url": "/img/background.4ac65b8e.png"
+  },
+  {
+    "revision": "3d19f3a7040f8445988b3627c2466341",
+    "url": "/img/globe.3d19f3a7.png"
+  },
+  {
+    "revision": "c54780969657fbd64bbdc9b0764c9e88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "252d7e2ca1d0624020fe",
+    "url": "/js/app.cf9307af.js"
+  },
+  {
+    "revision": "db5877375cfe95cfb9f4",
+    "url": "/js/chunk-vendors.4d1e4ca4.js"
+  },
+  {
+    "revision": "2a64b7e331c7811aefa6",
+    "url": "/js/message.efa970f5.js"
+  },
+  {
+    "revision": "b0e128b85fac9d4ef015",
+    "url": "/js/message:sign.ffa98d81.js"
+  },
+  {
+    "revision": "ea0dcd1eac132489aa0c",
+    "url": "/js/message:sign~message:verify.26d98f56.js"
+  },
+  {
+    "revision": "a79e3877b03cee110b3c",
+    "url": "/js/message:sign~message:verify~wallet:entropy~wallet:passphrase.67f365b8.js"
+  },
+  {
+    "revision": "d1b8a8a7e25f53d68537",
+    "url": "/js/message:sign~wallet:entropy~wallet:passphrase.c19c845f.js"
+  },
+  {
+    "revision": "d7452dd851606b09e836",
+    "url": "/js/message:verify.6204ce23.js"
+  },
+  {
+    "revision": "36809e6657759a5e2980",
+    "url": "/js/wallet.3b25fd1a.js"
+  },
+  {
+    "revision": "db68e031749c721f114f",
+    "url": "/js/wallet:entropy.dfd333ad.js"
+  },
+  {
+    "revision": "85e6ab69b302e26bdbfe",
+    "url": "/js/wallet:entropy~wallet:passphrase.036cee85.js"
+  },
+  {
+    "revision": "b0f5dc43fd5b39f094bf",
+    "url": "/js/wallet:passphrase.54f589ef.js"
+  },
+  {
+    "revision": "b3968197b9adc2264383742e7a6ce851",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
